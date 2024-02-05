1ef6e81a6fb258a4b2929a93c79ab252a598d24a
Aleksandr <aleksandr.evlanicov@gmail.com>
2024-02-05 12:16:43 +0300

    del

--- AuthorizationFinWorkGB/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthorizationFinWorkGB.Models
-{
-    public class Users
-    {
-        public int Id { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
